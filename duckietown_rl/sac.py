fixing: nan gradient; sol: clip gradient

duckietown_rl/sac.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 from torch.distributions import Normal
 import numpy as np
 import collections, random
+from utils import normalize, unnormalize, to_numpy
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -73,7 +74,6 @@ class ActorCNN(nn.Module):
 
         action[:, 0] = self.max_action * self.sigm(action[:, 0])  # because we don't want the duckie to go backwards
         action[:, 1] = self.tanh(action[:, 1])
-        action[torch.isnan(action)] = 0
         
         real_log_prob = log_prob - torch.log(1-torch.tanh(action).pow(2) + 1e-7)
         return action, real_log_prob
@@ -90,11 +90,13 @@ class ActorCNN(nn.Module):
         loss = -min_q - entropy # for gradient ascent
         self.optimizer.zero_grad()
         loss.mean().backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.optimizer.step()
 
-        self.log_alpha_optimizer.zero_grad()
         alpha_loss = -(self.log_alpha.exp() * (log_prob + target_entropy).detach()).mean()
+        self.log_alpha_optimizer.zero_grad()
         alpha_loss.backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.log_alpha_optimizer.step()
         
         return loss.mean(), alpha_loss
@@ -145,9 +147,10 @@ class CriticCNN(nn.Module):
 
     def train_net(self, target, mini_batch):
         s, a, r, s_prime, done = mini_batch
-        loss = F.smooth_l1_loss(self.forward(s, a) , target)
+        loss = F.smooth_l1_loss(self.forward(s, a), target)
         self.optimizer.zero_grad()
         loss.mean().backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.optimizer.step()
         return loss.mean()
     
@@ -160,7 +163,7 @@ def calc_target(pi, q1, q2, mini_batch):
 
     with torch.no_grad():
         a_prime, log_prob = pi(s_prime)
-        entropy = -pi.log_alpha.exp() * log_prob
+        entropy = torch.mean(-pi.log_alpha.exp() * log_prob)
         q1_val, q2_val = q1(s_prime,a_prime), q2(s_prime,a_prime)
         q1_q2 = torch.cat([q1_val, q2_val], dim=1)
         min_q = torch.min(q1_q2, 1, keepdim=True)[0]
@@ -169,21 +172,22 @@ def calc_target(pi, q1, q2, mini_batch):
     return target
 
 class SAC(object):
-    def __init__(self, state_dim, action_dim, max_action):
+    def __init__(self, state_dim, action_dim, max_action, learning_rate=lr_alpha, normalize_rew=True):
         super(SAC, self).__init__()
 
         self.state_dim = state_dim
         self.flat = False
 
-        self.actor = ActorCNN(action_dim, max_action, lr_alpha).to(device)
+        self.actor = ActorCNN(action_dim, max_action, learning_rate).to(device)
 
-        self.critic_1 = CriticCNN(action_dim, lr_alpha).to(device)
-        self.critic_1_target = CriticCNN(action_dim, lr_alpha).to(device)
-        self.critic_2 = CriticCNN(action_dim, lr_alpha).to(device)
-        self.critic_2_target = CriticCNN(action_dim, lr_alpha).to(device)
+        self.critic_1 = CriticCNN(action_dim, learning_rate).to(device)
+        self.critic_1_target = CriticCNN(action_dim, learning_rate).to(device)
+        self.critic_2 = CriticCNN(action_dim, learning_rate).to(device)
+        self.critic_2_target = CriticCNN(action_dim, learning_rate).to(device)
 
         self.critic_1_target.load_state_dict(self.critic_1.state_dict())
         self.critic_2_target.load_state_dict(self.critic_2.state_dict())
+        self.normalize_rew = normalize_rew
 
     def predict(self, state):
         # just making sure the state has the correct format, otherwise the prediction doesn't work
@@ -195,8 +199,10 @@ class SAC(object):
             state = torch.FloatTensor(np.expand_dims(state, axis=0)).to(device)
 
         state = state.detach()
-        action = self.actor(state)[0].cpu().data.numpy().flatten()
-
+        self.actor.eval()
+        with torch.no_grad():
+            action = to_numpy(self.actor(state)[0]).flatten()
+        self.actor.train()
         return action
 
     def train(self, replay_buffer, iterations, batch_size=64, discount=0.99, tau=0.001):
@@ -214,6 +220,11 @@ class SAC(object):
             done = torch.FloatTensor(1 - sample["done"]).to(device)
             reward = torch.FloatTensor(sample["reward"]).to(device)
 
+            if self.normalize_rew:
+                past_k_rewards = replay_buffer.get_reward()
+                reward_mean, reward_std = np.mean(past_k_rewards), np.std(past_k_rewards)
+                reward = normalize(reward, reward_mean, reward_std)
+
             # recombine into a minibatch 
             mini_batch = (state, action, reward, next_state, done)
 
@@ -223,19 +234,19 @@ class SAC(object):
             # train q1 and q2 
             critic_loss_1 = self.critic_1.train_net(td_target, mini_batch)
             critic_loss_2 = self.critic_2.train_net(td_target, mini_batch)
-            
+
             # calculate entropy 
             actor_loss, alpha_loss = self.actor.train_net(self.critic_1, self.critic_2, mini_batch)
-            
+
             # update critic
             self.critic_1.soft_update(self.critic_1_target)
             self.critic_2.soft_update(self.critic_2_target)
 
             # summarize losses
-            total_critic_1_loss += critic_loss_1.detach().cpu().numpy()
-            total_critic_2_loss += critic_loss_2.detach().cpu().numpy()
-            total_actor_loss += actor_loss.detach().cpu().numpy()
-            total_alpha_loss += alpha_loss.detach().cpu().numpy()
+            total_critic_1_loss += to_numpy(critic_loss_1)
+            total_critic_2_loss += to_numpy(critic_loss_2)
+            total_actor_loss += to_numpy(actor_loss)
+            total_alpha_loss += to_numpy(alpha_loss)
 
         return {
             "critic_loss_1": total_critic_1_loss / iterations, 

duckietown_rl/sac_rcrl.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 from torch.distributions import Normal
 import numpy as np
 import collections, random
+from utils import normalize, unnormalize, to_numpy
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -84,7 +85,7 @@ class ActorCNN(nn.Module):
         action[:, 0] = self.max_action * self.sigm(action[:, 0])  # because we don't want the duckie to go backwards
         action[:, 1] = self.tanh(action[:, 1])
 
-        action[torch.isnan(action)] = 0
+        # action[torch.isnan(action)] = self.max_action
 
         real_log_prob = log_prob - torch.log(1-torch.tanh(action).pow(2) + 1e-7)
         return action, real_log_prob
@@ -101,11 +102,13 @@ class ActorCNN(nn.Module):
         loss = -min_q - entropy # for gradient ascent
         self.optimizer.zero_grad()
         loss.mean().backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.optimizer.step()
 
-        self.log_alpha_optimizer.zero_grad()
         alpha_loss = -(self.log_alpha.exp() * (log_prob + target_entropy).detach()).mean()
+        self.log_alpha_optimizer.zero_grad()
         alpha_loss.backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.log_alpha_optimizer.step()
         
         return loss.mean(), alpha_loss
@@ -159,6 +162,7 @@ class CriticCNN(nn.Module):
         loss = F.smooth_l1_loss(self.forward(s, a) , target)
         self.optimizer.zero_grad()
         loss.mean().backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.optimizer.step()
         return loss.mean()
     
@@ -220,6 +224,7 @@ class PriorCNN(nn.Module):
        
         self.prior_optimizer.zero_grad()
         prior_loss.backward()
+        nn.utils.clip_grad_norm_(self.parameters(), 1.0)
         self.prior_optimizer.step()
         return prior_loss
 
@@ -228,7 +233,7 @@ def calc_target(pi, q1, q2, mini_batch):
 
     with torch.no_grad():
         a_prime, log_prob = pi(s_prime, prior)
-        entropy = -pi.log_alpha.exp() * log_prob
+        entropy = torch.mean(-pi.log_alpha.exp() * log_prob)
         q1_val, q2_val = q1(s_prime,a_prime), q2(s_prime,a_prime)
         q1_q2 = torch.cat([q1_val, q2_val], dim=1)
         min_q = torch.min(q1_q2, 1, keepdim=True)[0]
@@ -237,7 +242,7 @@ def calc_target(pi, q1, q2, mini_batch):
     return target
 
 class SACRCRL(object):
-    def __init__(self, state_dim, action_dim, max_action, prior_dim, lr_actor=1e-4, lr_critic=1e-3, lr_prior=1e-4):
+    def __init__(self, state_dim, action_dim, max_action, prior_dim, lr_actor=1e-4, lr_critic=1e-3, lr_prior=1e-4, normalize_rew=True):
         super(SACRCRL, self).__init__()
         self.state_dim = state_dim
         self.prior_dim = prior_dim 
@@ -255,6 +260,7 @@ class SACRCRL(object):
         self.critic_2_target.load_state_dict(self.critic_2.state_dict())
 
         self.prior_regressor = PriorCNN(prior_dim, lr_prior, output_activation='sigmoid').to(device)
+        self.normalize_rew = normalize_rew
 
     def predict(self, state):
         # just making sure the state has the correct format, otherwise the prediction doesn't work
@@ -266,9 +272,13 @@ class SACRCRL(object):
             state = torch.FloatTensor(np.expand_dims(state, axis=0)).to(device)
 
         state = state.detach()
-        prior_prediction = self.prior_regressor(state)
-        action = self.actor(state, prior_prediction)[0].cpu().data.numpy().flatten()
-
+        self.actor.eval()
+        self.prior_regressor.eval()
+        with torch.no_grad():
+            prior_prediction = self.prior_regressor(state)
+            action = to_numpy(self.actor(state, prior_prediction)[0]).flatten()
+        self.actor.train()
+        self.prior_regressor.train()
         return action
 
     def train(self, replay_buffer, iterations, batch_size=64, discount=0.99, tau=0.001):
@@ -291,6 +301,11 @@ class SACRCRL(object):
             prior_t = torch.FloatTensor(sample["additional"]).to(device)
             prior_t = torch.reshape(prior_t, (-1, self.prior_dim))
 
+            if self.normalize_rew:
+                past_k_rewards = replay_buffer.get_reward()
+                reward_mean, reward_std = np.mean(past_k_rewards), np.std(past_k_rewards)
+                reward = normalize(reward, reward_mean, reward_std)
+
             # recombine into a minibatch 
             mini_batch = (state, action, reward, next_state, done, prior_t)
 
@@ -312,11 +327,11 @@ class SACRCRL(object):
             self.critic_2.soft_update(self.critic_2_target)
 
             # summarize losses
-            total_prior_loss += prior_loss.detach().cpu().numpy()
-            total_critic_1_loss += critic_loss_1.detach().cpu().numpy()
-            total_critic_2_loss += critic_loss_2.detach().cpu().numpy()
-            total_actor_loss += actor_loss.detach().cpu().numpy()
-            total_alpha_loss += alpha_loss.detach().cpu().numpy()
+            total_prior_loss += to_numpy(prior_loss)
+            total_critic_1_loss += to_numpy(critic_loss_1)
+            total_critic_2_loss += to_numpy(critic_loss_2)
+            total_actor_loss += to_numpy(actor_loss)
+            total_alpha_loss += to_numpy(alpha_loss)
 
         return {
             "prior_loss": total_prior_loss / iterations,

duckietown_rl/scripts/train_cnn.py
@@ -57,7 +57,7 @@ def train(args):
     state_dim = env.observation_space.shape
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
-
+    print("reward normalization: ", args.normalize_rew)
 
     # Initialize policy
     if args.rcrl and args.sac:
@@ -65,7 +65,7 @@ def train(args):
     elif args.rcrl:
         policy = RCRL(state_dim, action_dim, max_action, prior_dim=args.prior_dim, lr_actor=args.lr_actor, lr_critic=args.lr_critic, lr_prior=args.lr_prior)
     elif args.sac:
-        policy = SAC(state_dim, action_dim, max_action)
+        policy = SAC(state_dim, action_dim, max_action, learning_rate=args.lr_actor, normalize_rew=args.normalize_rew)
     else: 
         policy = DDPG(state_dim, action_dim, max_action, net_type="cnn")
 
@@ -124,17 +124,18 @@ def train(args):
             episode_num += 1
 
         # Select action randomly or according to policy
-        if total_timesteps < args.start_timesteps:
-            action = env.action_space.sample()
-        else:
-            action = policy.predict(np.array(obs))
-            if args.expl_noise != 0:
-                action = (action + np.random.normal(
-                    0,
-                    args.expl_noise,
-                    size=env.action_space.shape[0])
-                        ).clip(env.action_space.low, env.action_space.high)
-
+        # if total_timesteps < args.start_timesteps:
+        #     action = env.action_space.sample()
+        # else:
+        #     action = policy.predict(np.array(obs))
+        #     if args.expl_noise != 0:
+        #         action = (action + np.random.normal(
+        #             0,
+        #             args.expl_noise,
+        #             size=env.action_space.shape[0])
+        #                 ).clip(env.action_space.low, env.action_space.high)
+        action = policy.predict(np.array(obs))
+        
         if args.rcrl: 
             current_world_objects = env.objects
             obj_distances = []
@@ -157,7 +158,13 @@ def train(args):
             else:
                 sample_prior = exp_neg_min_dist
         # Perform action
-        new_obs, reward, done, _ = env.step(action)
+        try:
+            new_obs, reward, done, _ = env.step(action)
+        except (ValueError, AssertionError):
+            print("action invalid: ")
+            print(action)
+            import pdb; pdb.set_trace()
+
         # if action[0] < 0.001:   # TODO: maybe have to increase this value to 0.01 or 0.1
         #     reward = 0
 

duckietown_rl/utils.py
@@ -20,18 +20,28 @@ class ReplayBuffer(object):
         self.storage = []
         self.max_size = max_size
         self.additional = additional 
+        self.rew = []
 
     def __len__(self):
         return len(self.storage)
+
+    def get_reward(self, up_to=5000): # was 1000
+        if len(self.rew):
+            return np.array(self.rew[:min(len(self), up_to)])
+        return np.array([0])
 
     # Expects tuples of (state, next_state, action, reward, done)
     def add(self, state, next_state, action, reward, done, additional=None):
         if len(self.storage) < self.max_size:
             self.storage.append((state, next_state, action, reward, done, additional))
+            self.rew.append(reward)
         else:
             # Remove random element in the memory beforea adding a new one
-            self.storage.pop(random.randrange(len(self.storage)))
+            idx = random.randrange(len(self.storage))
+            self.storage.pop(idx)
             self.storage.append((state, next_state, action, reward, done, additional))
+            self.rew.pop(idx)
+            self.rew.append(reward)
 
     def sample(self, batch_size=100, flat=True):
         ind = np.random.randint(0, len(self.storage), size=batch_size)
@@ -84,3 +94,12 @@ def evaluate_policy(env, policy, eval_episodes=10, max_timesteps=500):
     avg_reward /= eval_episodes
 
     return avg_reward
+
+def normalize(data, mean, std, eps=1e-8):
+    return (data-mean)/(std+eps)
+
+def unnormalize(data, mean, std):
+    return data*std+mean
+
+def to_numpy(tensor):
+    return tensor.to('cpu').detach().numpy()